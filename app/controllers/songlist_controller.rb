e357314ff4cccbee6164ef7736dc6d5e1a1b687b
齋木一晴 <120420issei@gmail.com>
2022-03-31 17:15:06 +0900

    曲追加機能完成

--- app/controllers/songlist_controller.rb
+++ app/controllers/songlist_controller.rb
@@ -1,9 +1,11 @@
 class SonglistController < ApplicationController
     def index
+        @songs = Song.all
     end
 
     def add_key
-        @song_title = params[:song_title]
+        @song = Song.new(song_title:params[:song_title])
+        @song.save
         redirect_to("/songlist")
     end
 end
