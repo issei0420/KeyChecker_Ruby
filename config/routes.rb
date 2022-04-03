e503d8111188fce8a3a37c8c4ab336c2f6410a5e
齋木一晴 <120420issei@gmail.com>
2022-04-03 18:23:10 +0900

    herokuテスト

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  get '/songlist/1/destroy' => "songlist#index"
   get '/' => "home#top"
   get '/keycheck' => "keycheck#top"
   post '/keycheck/check_key' => "keycheck#check_key"
-  post '/songlist/:id/destroy' => "songlist#destroy"
   get '/songlist' => "songlist#index"
   post '/songlist/add_key' => "songlist#add_key"
+  post '/songlist/:id/destroy' => "songlist#destroy"
 end
